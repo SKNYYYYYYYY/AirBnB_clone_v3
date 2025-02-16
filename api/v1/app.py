cf8977e37ec02e9d10bc07cf2f02a03e6ffb51d1
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-02-16 11:47:11 +0300

    created handler for 404 master

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Contains the flask web app
 """
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
 
@@ -18,6 +18,10 @@ def teardown_db(exception):
     """Delete all application"""
     storage.close()
 
+@app.errorhandler(404)
+def error_404(error):
+    """Error 404 response"""
+    return make_response(jsonify({"error": "Not Found"}))
 
 if __name__ == "__main__":
     hostt = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
