d7c565922b6424f35b39ca344095bf9ddfa48093
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-02-18 00:14:27 +0300

    updated user master

--- models/user.py
+++ models/user.py
@@ -27,3 +27,19 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if "password" in kwargs:
+            self.password = self.hash_password(kwargs["password"])
+
+    def hash_password(self, password):
+        """Hashes the password using MD5"""
+        return hashlib.md5(password.encode()).hexdigest()
+
+    @property
+    def password(self):
+        """Getter for password"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Setter for password that hashes it"""
+        self._password = self.hash_password(value) if value else None
